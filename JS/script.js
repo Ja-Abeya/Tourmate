1535b9d4b53b15bffe1677dbbe815ca85410f6d4
Jale B Abeya <jalebikila.abeya221@student.solbridge.ac.kr>
2024-10-11 18:16:55 +0900

    Initial commit of Tourmate

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// Simple script to show the website is working
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("TourMate website loaded successfully!");
+});
+
+// Additional common JavaScript functionality for future development can be placed here.
