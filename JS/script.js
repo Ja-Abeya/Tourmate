f6a63c965784b092da368e85f9a3335a82eb76e5
Jale B Abeya <144201751+Ja-Abeya@users.noreply.github.com>
2024-10-13 19:14:05 +0900

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 function initMap() {
     var location;
-    
+
+    // Identify the city based on the current page URL
     if (window.location.pathname.includes('busan.html')) {
         location = { lat: 35.1796, lng: 129.0756 }; // Busan coordinates
     } else if (window.location.pathname.includes('daegu.html')) {
@@ -11,6 +12,7 @@ function initMap() {
         location = { lat: 33.4996, lng: 126.5312 }; // Jeju coordinates
     }
 
+    // Create the map and marker for the specified location
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: location
